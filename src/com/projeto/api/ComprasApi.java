23de4b34a81463f2434150ba552f7a3c7054bea1
Adryan <adryantroian@gmail.com>
2026-05-26 12:05:52 -0300

    api busca por cod

--- src/com/projeto/api/ComprasApi.java
+++ src/com/projeto/api/ComprasApi.java
@@ -1,8 +1,11 @@
 package com.projeto.api;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,20 @@ public class ComprasApi {
     @ResponseBody
     public ResponseEntity<Iterable<ListaCompra>> listar(){
         return new ResponseEntity<Iterable<ListaCompra>>(this.listaCompraService.listar(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("{codigo}")
+    @ResponseBody
+    public ResponseEntity<ListaCompra> getPorCodigo(@PathVariable("codigo") int codigo) {
+        Optional<ListaCompra>opCompra = this.listaCompraService.getPorCodigo(codigo);
+        if (opCompra.isPresent()) {
+            return new ResponseEntity<ListaCompra>(opCompra.get(), HttpStatus.OK);
+        } 
+        
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
     }
     
